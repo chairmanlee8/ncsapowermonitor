13cbfc6e633007cc88548062bd6616d23ae31469
Andrew Lee <smiley325squared@gmail.com>
2011-10-28 16:29:29 -0500

    wip

--- source/powermon.cpp
+++ source/powermon.cpp
@@ -12,9 +12,10 @@ void powermon_config(powermon_config_t* pcfg, char* fn)
 
 	int cin = 0;
 	char buffer[256] = "";
-	char* dest = 0;
-	int buf_index = 0;
+	char dest[256] = "";
+	int buf_index = 0, dest_index = 0;
 	int state = 0;	// states are 0-discarding, 1-buffering, 2-expecting rval, 3-dest loading
+	int state_ivar = 0;
 
 	while((cin = fgetc(fp)) != EOF)
 	{
@@ -23,14 +24,48 @@ void powermon_config(powermon_config_t* pcfg, char* fn)
 			case 0:
 				if((char)cin == '\"')
 				{
-					
+					state = 1;
+					buf_index = 0;
 				}
 				break;
 			case 1:
+				if((char)cin == '\"')
+				{
+					buffer[buf_index++] = '\0';
+					state = 2;
+				}
+				else
+					buffer[buf_index++] = (char)cin;
 				break;
 			case 2:
+				if((char)cin == '\"')
+				{
+					state = 3;
+					dest_index = 0;
+				}
+				else if((char)cin == '{')
+				{
+					if(!strcmp(buffer, "instance_variables")) state_ivar = 1;
+				}
 				break;
 			case 3:
+				if((char)cin == '\"') 
+				{
+					dest[dest_index++] = '\0';
+
+					if(!strcmp(buffer, "db_host")) strcpy(pcfg->db_host, buffer);
+					else if(!strcmp(buffer, "db_port")) pcfg->db_port = atoi(buffer);
+					else if(!strcmp(buffer, "db_username")) strcpy(pcfg->db_username, buffer);
+					else if(!strcmp(buffer, "db_password")) strcpy(pcfg->db_password, buffer);
+					else if(!strcmp(buffer, "db_database")) strcpy(pcfg->db_database, buffer);
+					else if(!strcmp(buffer, "keep_alive_interval")) pcfg->keep_alive = atoi(buffer);
+					else if(!strcmp(buffer, "collect_host")) strcpy(pcfg->host, buffer);
+					else if(!strcmp(buffer, "collect_port")) pcfg->port = atoi(buffer);
+
+					state = 0;
+				}
+				else
+					dest[dest_index++] = (char)cin;
 				break;
 			default:
 				break;
