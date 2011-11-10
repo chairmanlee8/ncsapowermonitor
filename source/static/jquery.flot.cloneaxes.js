4a2cffb37b7a8f136757bba60dd920b72f30271d
Andrew Lee <smiley325squared@gmail.com>
2011-11-10 14:10:40 -0600

    wip

--- source/static/jquery.flot.cloneaxes.js
+++ source/static/jquery.flot.cloneaxes.js
@@ -1,7 +1,7 @@
 (function ($) {
 	function init(plot) {
 		plot.cloneAxes = function (templatePlot) {
-			var options = templatePlot.getOptions();
+			/*var options = templatePlot.getOptions();
 			var axes = templatePlot.getAxes();
 			var targetOptions = plot.getOptions();
 			var targetAxes = plot.getAxes();
@@ -10,17 +10,18 @@
 			for(var k in props)
 			{
 				if(!targetAxes[props[k]].used) continue;
-
 				targetOptions[props[k]].min = axes[props[k]].min;
 				targetOptions[props[k]].max = axes[props[k]].max;
 			}
 
 			console.log(plot.getPlaceholder().attr('id'));
-			console.log(plot.getAxes()['xaxis'].min);
+			console.log(plot.getAxes()['xaxis'].min);*/
+
+			plot.xaxes = templatePlot.getXAxes();
+			plot.yaxes = templatePlot.getYAxes();
 
 			plot.setupGrid();
 			console.log(plot.getAxes()['xaxis'].min);
-			console.log("setup");
 			plot.draw();
 		}
 	}
